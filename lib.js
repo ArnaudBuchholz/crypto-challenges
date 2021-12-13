d0abe73a08f48db99162ebf15d899cf602e1bf9d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-13 14:55:13 -0500

    Common DOM helper

---
+++ lib.js
@@ -0,0 +1,17 @@
+'use strict'
+
+window.tag = (name, attributes = {}, ...children) => {
+  const node = document.createElement(name)
+  Object.keys(attributes).forEach(name => {
+    node.setAttribute(name, attributes[name])
+  })
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child)
+    }
+    if (child) {
+      node.appendChild(child)
+    }
+  })
+  return node
+}
